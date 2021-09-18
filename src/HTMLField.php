ee3a311516ca60c2816fc1a8dbbd9a4853d2e53b
Tommi Kärkkäinen <tommikarkkainen@users.noreply.github.com>
2021-09-18 15:15:10 +0300

    Make HTML Fields display correctly

--- src/HTMLField.php
+++ src/HTMLField.php
@@ -1,5 +1,9 @@
 <?php
 
+/*!
+ * HTMLField is not a *real* form field, but allows adding arbitrary HTML
+ * (e.g. for explanatory texts) into the form.
+ */
 class HTMLField extends FormField
 {
     public function __construct(object $obj)
@@ -7,6 +11,11 @@ class HTMLField extends FormField
         $this->name = $obj->name;
         $this->label = $obj->label;
         $this->type = $obj->type;
+    }
+
+    public function makeField(): TagFactory
+    {
+        return new TextElement($this->label);
     }
 }
 
