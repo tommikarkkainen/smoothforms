10de2b142be887046596cbf4171e42eec7c62304
Tommi Kärkkäinen <tommikarkkainen@users.noreply.github.com>
2021-09-23 20:53:47 +0300

    Improve SelectField behavior with submitted data

--- src/SelectField.php
+++ src/SelectField.php
@@ -6,6 +6,7 @@
 class SelectField extends InputField
 {
     protected array $options;
+    private bool $strict_mode;
 
     public function __construct(object $obj)
     {
@@ -18,6 +19,15 @@ class SelectField extends InputField
             $this->options = $obj->options;
         } else {
             $this->options = array();
+        }
+
+        $this->strict_mode = false;
+        if(property_exists($obj, "strictmode"))
+        {
+            if( (gettype($obj->strictmode) == "boolean" && $obj->strictmode == true)
+                || (gettype($obj->strictmode) == "string" && 
+                strcmp(strtolower($obj->strictmode), "true") == 0 ))
+                $this->strict_mode = true;
         }
     }
 
@@ -60,6 +70,55 @@ class SelectField extends InputField
         $this->makeValidatorErrors($field);
         
         return $field;
+    }
+
+    /*!
+     * Read the value submitted to this field, from the POST array. Write the
+     * value into the $value member variable.
+     * 
+     * If the field is a select field, or if the field is a datalist AND it has
+     * strict mode enabled, automatically check that the selected option is one
+     * one of the allowed values. If not, the value is set as an empty string
+     * that can be caught by a RequiredValidator if desired.
+     */
+    protected function readSubmissionValue()
+    {
+        if(isset($_POST[$this->name]))
+        {
+            if($this->type=="select" || $this->strict_mode)
+            {
+                $this->value = $this->getStrictValue($_POST[$this->name]);
+            } else {
+                $this->value = filter_var($_POST[$this->name],
+                    FILTER_SANITIZE_STRING);
+            }
+        } else {
+            $this->value = "";
+        }
+    }
+
+    /*!
+     * Read the user-input value in strict mode: only accept a value listed in
+     * the option list of this field. Substitute other values with "".
+     */    
+    private function getStrictValue(string $value): string
+    {
+        $str = "";
+        foreach($this->options as $option)
+        {
+            if($option->value == $value)
+            {
+                if($this->type == "select")
+                    $str = $option->label;
+                if($this->type == "datalist")
+                    $str = $option->value;
+
+                break;
+            }
+        }
+
+        $str = filter_var($str, FILTER_SANITIZE_STRING);
+        return $str;
     }
 }
 
