a52884c13dd220090ea363030590c4eb853ebabe
Tommi Kärkkäinen <tommikarkkainen@users.noreply.github.com>
2021-09-30 19:07:43 +0300

    Create a basic unit test framework

---
+++ tests/TestRequiredValidator.php
@@ -0,0 +1,21 @@
+<?php
+/*!
+ * Run all unit tests. When a new test file is added, add a call in this file.
+ */
+
+try
+{
+    $required_validator = new RequiredValidator("invalid");
+} catch(Exception $e) {}
+
+assert(!isset($required_validator));
+
+$required_validator = new RequiredValidator("required");
+assert($required_validator instanceof RequiredValidator);
+assert($required_validator->isValid(null) == false);
+assert($required_validator->isValid("") == false);
+assert($required_validator->isValid(" ") == false);
+assert($required_validator->isValid(125));
+assert($required_validator->isValid("This is a string."));
+
+?>
